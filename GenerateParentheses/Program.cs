f2824843daa40b80b1db9fee10fc0c8be39700bd
Deyan Diulgerov <chukstv@abv.bg>
2023-06-15 11:53:44 +0300

    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- GenerateParentheses/Program.cs
+++ GenerateParentheses/Program.cs
@@ -6,10 +6,33 @@ using System.Threading.Tasks;
 
 namespace GenerateParentheses
 {
-    internal class Program
-    {
-        static void Main(string[] args)
+     static void Main(string[] args)
         {
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine(String.Join(",", GenerateParenthesis(3)));
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine(String.Join(",", GenerateParenthesis(1)));
+            Console.WriteLine("---------------------------------------");
         }
-    }
+
+        public static IList<string> GenerateParenthesis(int n)
+        {
+            var list = new List<string>();
+            Gen(0, 0, "");
+            return list;
+
+            void Gen(int i, int j, string curr)
+            {
+                if(i >= n && j >= n)
+                {
+                    list.Add(curr);
+                    return;
+                }
+
+                if (j < i)
+                    Gen(i, j + 1, $"{curr})");
+                if (i < n)
+                    Gen(i + 1, j, $"{curr}(");
+            }
+        }
 }
