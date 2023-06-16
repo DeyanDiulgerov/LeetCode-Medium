0db48a67535d9d9d4c8a39c067c17629bb1cba4c
Deyan Diulgerov <chukstv@abv.bg>
2023-06-16 12:46:58 +0300

    Add files via upload

---
+++ SearchRotatedSortedArray/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchRotatedSortedArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SearchRotatedSortedArray(new int[] { 4, 5, 6, 7, 0, 1, 2 }, 0));
+            Console.WriteLine(SearchRotatedSortedArray(new int[] { 4, 5, 6, 7, 0, 1, 2 }, 3));
+            Console.WriteLine(SearchRotatedSortedArray(new int[] { 1 }, 0));
+        }
+
+        public static int SearchRotatedSortedArray(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Count(); i++)
+            {
+                if (nums[i] == target)
+                    return i;
+            }
+
+            return -1;
+        }
+    }
+}
