808927368c4b154cf91914d6d147544142f359f4
Deyan Diulgerov <chukstv@abv.bg>
2023-06-27 13:17:12 +0300

    Add files via upload

---
+++ ShortestDistanceToChar/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShortestDistanceToChar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(String.Join(",", ShortestDistanceToChar("abaa", 'b')));
+            Console.WriteLine(String.Join(",", ShortestDistanceToChar("loveleetcode", 'e')));
+            Console.WriteLine(String.Join(",", ShortestDistanceToChar("aaab", 'b')));
+        }
+
+
+        public static int[] ShortestDistanceToChar(string s, char c)
+        {
+            var retList = new List<int>(s.Length);
+            int counter = 0;
+            int secondCounter = 0;
+            bool isFirstFound = false;
+            bool isSecondFound = false;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                counter = 0;
+                secondCounter = 0;
+                isSecondFound = false;
+                isFirstFound = false;
+
+                for (int j = i; j < s.Length; j++)
+                {
+                    if (s[j] != c)
+                        counter++;
+                    else
+                    {
+                        isFirstFound = true;
+                        break;
+                    }
+                }
+
+                for (int k = i; k >= 0; k--)
+                {
+                    if (s[k] != c)
+                        secondCounter++;
+                    else if (s[k] == c)
+                    {
+                        isSecondFound = true;
+                        break;
+                    }
+                }
+                if (isSecondFound == false)
+                    retList.Add(counter);
+                else if (isFirstFound == false)
+                    retList.Add(secondCounter);
+                else
+                {
+                    counter = Math.Min(counter, secondCounter);
+                    retList.Add(counter);
+                }
+            }
+
+            return retList.ToArray();
+        }
+    }
+}
