9c6ac53ee532c7c70be638c463441708464accd5
Deyan Diulgerov <chukstv@abv.bg>
2023-07-10 14:50:05 +0300

    Add files via upload

---
+++ FindTheOriginalArrayOfPrefixXOR/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindTheOriginalArrayOfPrefixXOR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(String.Join(",", FindTheOriginalArrayOfPrefixXOR(new int[] { 5, 2, 0, 3, 1 })));
+            Console.WriteLine(String.Join(",", FindTheOriginalArrayOfPrefixXOR(new int[] { 13 })));
+        }
+
+        public static int[] FindTheOriginalArrayOfPrefixXOR(int[] pref)
+        {
+            int n = pref.Length;
+            var result = new List<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                if (i == 0)
+                {
+                    int num = pref[i];
+                    result.Add(num);
+                }
+                if (i < n - 1)
+                {
+                    int num = pref[i] ^ pref[i + 1];
+                    result.Add(num);
+                }
+            }
+
+            return result.ToArray();
+        }
+    }
+}
