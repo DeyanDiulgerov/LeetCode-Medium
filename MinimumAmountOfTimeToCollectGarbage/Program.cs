3e1c2a752908b429d433b80ed6abec215b9b6684
Deyan Diulgerov <chukstv@abv.bg>
2023-07-11 17:09:41 +0300

    Add files via upload

---
+++ MinimumAmountOfTimeToCollectGarbage/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinimumAmountOfTimeToCollectGarbage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MinimumAmountOfTimeToCollectGarbage
+                (new string[] { "G", "G" }, new int[] { 1 }));
+
+            Console.WriteLine(MinimumAmountOfTimeToCollectGarbage
+                (new string[] { "G", "P", "GP", "GG" }, new int[] { 2, 4, 3 }));
+
+            Console.WriteLine(MinimumAmountOfTimeToCollectGarbage
+                (new string[] { "MMM", "PGM", "GP" }, new int[] { 3, 10 }));
+        }
+
+        public static int MinimumAmountOfTimeToCollectGarbage(string[] garbage, int[] travel)
+        {
+            int lastIndexM = 0, lastIndexP = 0, lastIndexG = 0;
+            int minutes = 0;
+
+            for (var i = 0; i < garbage.Length; i++)
+            {
+                minutes += garbage[i].Length; // |6| + 2 + 2 + 4 + 4 + 3
+                if (garbage[i].Contains('M'))
+                    lastIndexM = i; // 0
+                if (garbage[i].Contains('P'))
+                    lastIndexP = i; // 2
+                if (garbage[i].Contains('G'))
+                    lastIndexG = i; // 3
+            }
+
+            for (var j = 1; j < garbage.Length; j++) // 3
+            {
+                if (lastIndexM >= j)
+                    minutes += travel[j - 1];
+                if (lastIndexP >= j)
+                    minutes += travel[j - 1];
+                if (lastIndexG >= j)
+                    minutes += travel[j - 1];
+            }
+
+            return minutes;
+        }
+    }
+}
