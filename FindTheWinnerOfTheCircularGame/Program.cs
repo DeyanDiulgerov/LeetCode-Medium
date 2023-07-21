70a83a970240fc4b2f9a7aa9c14b341f45defa86
Deyan Diulgerov <chukstv@abv.bg>
2023-07-21 16:35:51 +0300

    !!!FAV!!!
    Use Queue to circle around from 5 to 1

--- FindTheWinnerOfTheCircularGame/Program.cs
+++ FindTheWinnerOfTheCircularGame/Program.cs
@@ -15,7 +15,10 @@ namespace FindTheWinnerOfTheCircularGame
         }
 
         public static int FindTheWinnerOfTheCircularGame(int n, int k)
-        {
+        {   
+             // first circle from 1 to n == 1 to 5
+            // Use Queue to circle around from 5 to 1
+            
             var queue = new Queue<int>();
             for (int i = 1; i <= n; i++)
                 queue.Enqueue(i);
