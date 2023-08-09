67b8ffebb4dc851b26aefb06285f57c7e4e6b853
Deyan Diulgerov <chukstv@abv.bg>
2023-08-09 14:45:08 +0300

    Add files via upload

---
+++ UniquePaths/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniquePaths
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(UniquePaths(3, 7));
+            Console.WriteLine(UniquePaths(3, 2));
+        }
+
+        public static int UniquePaths(int m, int n)
+        {
+            long result = 1;
+            for (int i = 1; i < m; i++)
+            {
+                result *= n + i - 1;
+                result /= i;
+            }
+            return (int)result;
+        }
+    }
+}
