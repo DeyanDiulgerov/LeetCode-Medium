6bd0c110224e8311c6edaf71bb8813b6de8d8cc7
Deyan Diulgerov <chukstv@abv.bg>
2023-08-11 17:27:25 +0300

    Add files via upload

---
+++ LargestNumber/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LargestNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(LargestNumber(new int[] { 10, 2 }));
+            Console.WriteLine(LargestNumber(new int[] { 3, 30, 34, 5, 9 }));
+        }
+
+        public static string LargestNumber(int[] nums)
+        {
+            return string.Join("", nums.Select(x => x.ToString())
+                        .OrderBy(s => s,
+                        Comparer<string>.Create((x, y) => string.Compare(y + x, x + y))))
+                     .TrimStart('0')
+                     .PadLeft(1, '0');
+        }
+    }
+}
