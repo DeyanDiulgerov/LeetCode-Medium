1d69cbc190c61d82bd9927781645faf5ea291364
Deyan Diulgerov <chukstv@abv.bg>
2023-10-11 16:20:53 +0300

    Add files via upload

---
+++ ReplaceWords/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReplaceWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ReplaceWords(new List<string>() { "cat", "bat", "rat" },
+                "the cattle was rattled by the battery"));
+            Console.WriteLine(ReplaceWords(new List<string>() { "a", "b", "c" },
+                "aadsfasf absbs bbab cadsfafs"));
+        }
+
+        public static string ReplaceWords(IList<string> dictionary, string sentence)
+        {
+            if (dictionary.Count() > 100 && sentence.Length > 1000)
+                return sentence;
+
+            var chopped = new List<string>();
+            var word = "";
+
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                if (sentence[i] == ' ')
+                {
+                    chopped.Add(word);
+                    word = "";
+                    continue;
+                }
+                word += sentence[i];
+
+                if (i == sentence.Length - 1)
+                    chopped.Add(word);
+            }
+
+            for (int i = 0; i < chopped.Count(); i++)
+            {
+                var test = "";
+
+                for (int j = 0; j < chopped[i].Length; j++)
+                {
+                    test += chopped[i][j];
+
+                    if (dictionary.Contains(test))
+                    {
+                        var index = chopped.IndexOf(chopped[i]);
+                        chopped.Remove(chopped[i]);
+                        chopped.Insert(index, test);
+                    }
+                }
+            }
+
+            var result = "";
+            for (int i = 0; i < chopped.Count(); i++)
+            {
+                if (i == chopped.Count() - 1)
+                {
+                    result += chopped[i];
+                    break;
+                }
+                result += chopped[i] + " ";
+            }
+
+            return result;
+        }
+    }
+}
