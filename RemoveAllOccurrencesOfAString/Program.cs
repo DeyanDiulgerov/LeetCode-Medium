f96f97db5b5c8fa1e9263b0414e71dc48b22c443
Deyan Diulgerov <chukstv@abv.bg>
2023-10-24 15:31:00 +0300

    Add files via upload

---
+++ RemoveAllOccurrencesOfAString/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveAllOccurrencesOfAString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(RemoveAllOccurrencesOfAString("daabcbaabcbc", "abc"));
+            Console.WriteLine(RemoveAllOccurrencesOfAString("axxxxyyyyb", "xy"));
+        }
+        public static string RemoveAllOccurrencesOfAString(string s, string part)
+        {
+            while (s.Contains(part))
+            {
+                var index = s.IndexOf(part);
+                s = s.Remove(index, part.Length);
+            }
+
+            return s;
+        }
+    }
+}
