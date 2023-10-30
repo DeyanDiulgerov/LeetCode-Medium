e41b8492092559601a1d6a8b6e81c9312d60ce42
Deyan Diulgerov <chukstv@abv.bg>
2023-10-30 13:18:19 +0200

    Add files via upload

---
+++ MaximumMatchingOfPlayersWithTrainers/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaximumMatchingOfPlayersWithTrainers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MaximumMatchingOfPlayersWithTrainers(
+                new int[] { 1, 1000000000 },
+                new int[] { 1000000000, 1 }));
+            Console.WriteLine(MaximumMatchingOfPlayersWithTrainers(new int[] { 4, 7, 9 }, new int[] { 8, 2, 5, 8 }));
+            Console.WriteLine(MaximumMatchingOfPlayersWithTrainers(new int[] { 1, 1, 1 }, new int[] { 10 }));
+        }
+
+        public static int MaximumMatchingOfPlayersWithTrainers(int[] players, int[] trainers)
+        {
+            var listedTrainers = new List<int>(trainers);
+            listedTrainers.Sort();
+            Array.Sort(players);
+            int counter = 0;
+
+            for (int i = 0; i < players.Length; i++)
+            {
+                if (listedTrainers.Count() == 0)
+                    break;
+
+                var coach = listedTrainers.FirstOrDefault(x => x >= players[i]);
+                if (coach != 0)
+                {
+                    listedTrainers.Remove(coach);
+                    counter++;
+                }
+            }
+
+            return counter;
+        }
+    }
+}
