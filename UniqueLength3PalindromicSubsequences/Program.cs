577aa8231d3bd432f4073b3a5c87b3e9d359f327
Deyan Diulgerov <chukstv@abv.bg>
2023-11-14 15:22:34 +0200

    Add files via upload

---
+++ UniqueLength3PalindromicSubsequences/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniqueLength3PalindromicSubsequences
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(String.Join(",", UniqueLength3PalindromicSubsequences("aabca")));
+            Console.WriteLine(String.Join(",", UniqueLength3PalindromicSubsequences("adc")));
+            Console.WriteLine(String.Join(",", UniqueLength3PalindromicSubsequences("bbcbaba")));
+        }
+
+        public static int UniqueLength3PalindromicSubsequences(string s)
+        {
+            var sum = 0;
+            for (char ch = 'a'; ch <= 'z'; ch++)
+            {
+                var firstIndex = s.IndexOf(ch);
+                var lastIndex = s.LastIndexOf(ch);
+                var hashSet = new HashSet<char>();
+
+                for (int i = firstIndex + 1; i < lastIndex; i++)
+                    hashSet.Add(s[i]);
+
+                sum += hashSet.Count();
+            }
+            return sum;
+        }
+    }
+}
