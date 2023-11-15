dadfac1e2293a188aca9bc63bdc56dc8d1b4d9e7
Deyan Diulgerov <chukstv@abv.bg>
2023-11-15 16:31:46 +0200

    Update Program.cs

--- BestTimeToBuyAndSellStockII/Program.cs
+++ BestTimeToBuyAndSellStockII/Program.cs
@@ -16,14 +16,14 @@ namespace BestTimeToBuyAndSellStockII
         }
         public static int BestTimeToBuyAndSellStockII(int[] prices)
         {
-            int max = 0;
+            int profit = 0;
 
             for (int i = 1; i < prices.Length; i++)
             {
                 if (prices[i] > prices[i - 1])
-                    max += prices[i] - prices[i - 1];
+                    profit += prices[i] - prices[i - 1];
             }
-            return max;
+            return profit;
         }
     }
 }
