7d7ff398e12c10d570edf3a3a615675bcc692ba8
Deyan Diulgerov <chukstv@abv.bg>
2023-11-28 14:48:12 +0200

    Added 2nd Sliding Window approach BUT worse performance

--- LongestSubarrayOf1sAfterDeletingOneElement/Program.cs
+++ LongestSubarrayOf1sAfterDeletingOneElement/Program.cs
@@ -21,6 +21,7 @@ namespace LongestSubarrayOf1sAfterDeletingOneElement
             Console.WriteLine(LongestSubarrayOf1sAfterDeletingOneElement(new int[] { 1, 1, 0, 0, 1, 1, 1, 0, 1 }));
         }
 
+        //1st Improvised ??? even Idk what approach 
         public static int LongestSubarrayOf1sAfterDeletingOneElement(int[] nums)
         {
             if (!nums.Any(x => x == 0))
@@ -72,6 +73,45 @@ namespace LongestSubarrayOf1sAfterDeletingOneElement
             }
             max = Math.Max(max, counter);
 
+            return max;
+        }
+        //2nd-Sliding Window approach BUT worse performance
+        public static int LongestSubarrayOf1sAfterDeletingOneElement(int[] nums)
+        {
+            if (nums.All(x => x == 1))
+                return nums.Length - 1;
+            if (nums.All(x => x == 0))
+                return 0;
+            int left = 0, right = 0;
+            int max = 0;
+            var tempRight = 0;
+
+            while (right < nums.Length)
+            {
+                right = left + 1;
+                while (left < nums.Length && nums[left] != 1)
+                    left++;
+                while (right <= left || (right < nums.Length && nums[right] == 1))
+                    right++;
+                if (left >= right || right >= nums.Length)
+                {
+                    // first time we cycle through
+                    if (tempRight == 0)
+                        max = Math.Max(max, right - left);
+
+                    break;
+                }
+
+                tempRight = right;
+                nums[right] = 1;
+                while (right < nums.Length && nums[right] == 1)
+                    right++;
+
+                nums[tempRight] = 0;
+                max = Math.Max(max, right - left - 1);
+                left++;
+            }
+            max = Math.Max(max, right - left - 1);
             return max;
         }
     }
