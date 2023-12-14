43f1eed682011d6a564e22859c3eaed58c72b9a9
Deyan Diulgerov <chukstv@abv.bg>
2023-12-14 14:54:22 +0200

    Add files via upload

---
+++ HIndex/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIndex
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(HIndex(new int[] { 3, 0, 6, 1, 5 }));
+            Console.WriteLine(HIndex(new int[] { 1, 3, 1 }));
+        }
+        public static int HIndex(int[] citations)
+        {
+            int n = citations.Length;
+
+            while (n > 0)
+            {
+                int count = citations.Count(x => x >= n);
+                if (count >= n)
+                    return n;
+                else
+                    n--;
+            }
+            return n;
+        }
+    }
+}
