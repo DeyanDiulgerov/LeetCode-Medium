b61a04f90e58456fac6a227670cfe5f1ecd0278b
Deyan Diulgerov <chukstv@abv.bg>
2023-12-15 17:34:21 +0200

    Add files via upload

---
+++ MaxIncreaseToKeepingCitySkyline/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxIncreaseToKeepingCitySkyline
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var grid1 = new int[][]
+            {
+                new int[] {3,0,8,4 },
+                new int[] {2,4,5,7 },
+                new int[] {9,2,6,3 },
+                new int[] {0,3,1,0 },
+            };
+            var grid2 = new int[][]
+            {
+                new int[] {0,0,0},
+                new int[] {0,0,0},
+                new int[] {0,0,0},
+            };
+            Console.WriteLine(MaxIncreaseToKeepingCitySkyline(grid1));
+            Console.WriteLine(MaxIncreaseToKeepingCitySkyline(grid2));
+        }
+        public static int MaxIncreaseToKeepingCitySkyline(int[][] grid)
+        {
+            int n = grid.Length;
+            int resultCount = 0;
+
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    int maxRow = 0;
+                    int maxCol = 0;
+
+                    for (int r = 0; r < n; r++)
+                    {
+                        int curr = grid[r][col];
+                        maxRow = Math.Max(maxRow, curr);
+                    }
+                    for (int c = 0; c < n; c++)
+                    {
+                        int curr = grid[row][c];
+                        maxCol = Math.Max(maxCol, curr);
+                    }
+
+                    int skyline = Math.Min(maxRow, maxCol);
+                    int blocksNeeded = skyline - grid[row][col];
+                    resultCount += blocksNeeded;
+                    grid[row][col] = skyline;
+                }
+            }
+            return resultCount;
+        }
+    }
+}
---
+++ MaxIncreaseToKeepingCitySkyline/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MaxIncreaseToKeepingCitySkyline")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MaxIncreaseToKeepingCitySkyline")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("45ca20c3-a001-477a-bdfd-a4e71ba4229b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
