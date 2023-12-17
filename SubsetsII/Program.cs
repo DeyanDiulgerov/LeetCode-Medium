9c91060a056492be8f21df480406de66607119df
Deyan Diulgerov <chukstv@abv.bg>
2023-12-17 14:19:44 +0200

    Add files via upload

---
+++ SubsetsII/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubsetsII
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var item in SubsetsII(new int[] { 4, 4, 4, 1, 4 }))
+                Console.WriteLine(String.Join(",", item));
+            foreach (var item in SubsetsII(new int[] { 1, 2, 2 }))
+                Console.WriteLine(String.Join(",", item));
+            foreach (var item in SubsetsII(new int[] { 0 }))
+                Console.WriteLine(String.Join(",", item));
+            foreach (var item in SubsetsII(new int[] { 1, 2, 3, 4 }))
+                Console.WriteLine(String.Join(",", item));
+        }
+
+        public static IList<IList<int>> SubsetsII(int[] nums)
+        {
+            var result = new List<IList<int>>();
+            result.Add(new List<int>() { });
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int count = result.Count();
+                for (int j = 0; j < count; j++)
+                {
+                    var temp = result[j].ToList();
+                    temp.Add(nums[i]);
+                    temp.Sort();
+                    if (!result.Any(x => x.SequenceEqual(temp)))
+                        result.Add(temp);
+                }
+            }
+            return result;
+        }
+    }
+}
