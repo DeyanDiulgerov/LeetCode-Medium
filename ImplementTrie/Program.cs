099dd791ba47a73758177a599ac1aae163d5e332
Deyan Diulgerov <chukstv@abv.bg>
2023-12-28 15:17:05 +0200

    Add files via upload

---
+++ ImplementTrie/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImplementTrie
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Trie trie = new Trie();
+            trie.Insert("apple");
+            trie.Search("apple");
+            trie.Search("app");
+            trie.StartsWith("app");
+            trie.Insert("app");
+            trie.Search("app");
+        }
+        public class Trie
+        {
+            List<string> allWords = new List<string>();
+            public Trie()
+            {
+                allWords = new List<string>();
+            }
+
+            public void Insert(string word)
+            {
+                Console.WriteLine(word);
+                allWords.Add(word);
+            }
+
+            public bool Search(string word)
+            {
+                Console.WriteLine(allWords.Contains(word));
+                return allWords.Contains(word);
+            }
+
+            public bool StartsWith(string prefix)
+            {
+                Console.WriteLine(allWords.Any(x => x.StartsWith(prefix)));
+                return allWords.Any(x => x.StartsWith(prefix));
+            }
+        }
+    }
+}
