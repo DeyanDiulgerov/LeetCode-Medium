171393f144a881ff8baaacad22781646a045ea11
Deyan Diulgerov <chukstv@abv.bg>
2023-12-28 13:24:20 +0200

    Add files via upload

---
+++ StringCompression/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCompression
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(StringCompression(new char[] { 'a', 'a' }));
+            Console.WriteLine(StringCompression(new char[] { 'a', 'a', 'b', 'b', 'c', 'c', 'c' }));
+            Console.WriteLine(StringCompression(new char[] { 'a' }));
+            Console.WriteLine(StringCompression(new char[]
+            {'a', 'b', 'b', 'b','b', 'b', 'b','b', 'b', 'b','b', 'b', 'b',}));
+        }
+        public static int StringCompression(char[] chars)
+        {
+            string newS = String.Concat(chars);
+            newS = GetCompressed(newS);
+
+            List<char> result = new List<char>();
+            for (int i = 0; i < newS.Length; i++)
+            {
+                result.Add(newS[i]);
+            }
+
+            int takeCount = 1;
+            for (int i = 0; i < chars.Length; i++)
+            {
+                if (i >= result.Count())
+                {
+                    takeCount = i;
+                    break;
+                }
+                chars[i] = result[i];
+                takeCount = i + 1;
+            }
+            if (takeCount == 0)
+                takeCount = 1;
+
+            return chars.Take(takeCount).Count();
+        }
+        public static string GetCompressed(string s)
+        {
+            string compressed = "";
+            for (int i = 0; i < s.Length; i++)
+            {
+                int letterCount = 1;
+                for (int j = i + 1; j < s.Length; j++)
+                {
+                    if (s[i] != s[j])
+                        break;
+
+                    letterCount++;
+                    i = j;
+                }
+
+                compressed += s[i];
+                if (letterCount != 1)
+                    compressed += letterCount;
+            }
+            return compressed;
+        }
+    }
+}
