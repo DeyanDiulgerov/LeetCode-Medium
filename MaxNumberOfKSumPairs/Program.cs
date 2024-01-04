a8ee486a19194bb2939e827a0a2180a2d3d1a538
Deyan Diulgerov <chukstv@abv.bg>
2024-01-04 12:33:20 +0200

    2 Approaches - Two Pointers && seen Nums List + diff

--- MaxNumberOfKSumPairs/Program.cs
+++ MaxNumberOfKSumPairs/Program.cs
@@ -10,11 +10,16 @@ namespace MaxNumberOfKSumPairs
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(MaxNumberOfKSumPairs(new int[] { 3, 1, 5, 1, 1, 1, 1, 1, 2, 2, 3, 2, 2 }, 1));
-            Console.WriteLine(MaxNumberOfKSumPairs(new int[] { 1, 2, 3, 4 }, 5));
-            Console.WriteLine(MaxNumberOfKSumPairs(new int[] { 3, 1, 3, 4, 3 }, 6));
+            Console.WriteLine(MaxNumberOfKSumPairsDiff(new int[] { 3, 1, 5, 1, 1, 1, 1, 1, 2, 2, 3, 2, 2 }, 1));
+            Console.WriteLine(MaxNumberOfKSumPairsDiff(new int[] { 1, 2, 3, 4 }, 5));
+            Console.WriteLine(MaxNumberOfKSumPairsDiff(new int[] { 3, 1, 3, 4, 3 }, 6));
+
+            Console.WriteLine(MaxNumberOfKSumPairsTwoPointer(new int[] { 3, 1, 5, 1, 1, 1, 1, 1, 2, 2, 3, 2, 2 }, 1));
+            Console.WriteLine(MaxNumberOfKSumPairsTwoPointer(new int[] { 1, 2, 3, 4 }, 5));
+            Console.WriteLine(MaxNumberOfKSumPairsTwoPointer(new int[] { 3, 1, 3, 4, 3 }, 6));
         }
-        public static int MaxNumberOfKSumPairs(int[] nums, int k)
+        // seenNums List And diff == k - nums[i] approach
+        public static int MaxNumberOfKSumPairsDiff(int[] nums, int k)
         {
             var usedNums = new List<int>();
             int counter = 0;
@@ -30,6 +35,31 @@ namespace MaxNumberOfKSumPairs
                     usedNums.Add(nums[i]);
             }
             return counter;
+        }
+        // Two Pointer Approach
+        public static int MaxOperationsTwoPointer(int[] nums, int k)
+        {
+            Array.Sort(nums);
+            var listed = new List<int>(nums);
+            int left = 0, right = listed.Count() - 1;
+            int operations = 0;
+
+            while (left < right)
+            {
+                if (listed[left] + listed[right] == k)
+                {
+                    operations++;
+                    listed.RemoveAt(right);
+                    listed.RemoveAt(left);
+                    left = 0;
+                    right = listed.Count() - 1;
+                }
+                else if (listed[left] + listed[right] < k)
+                    left++;
+                else //nums[left] + nums[right] > k
+                    right--;
+            }
+            return operations;
         }
     }
 }
