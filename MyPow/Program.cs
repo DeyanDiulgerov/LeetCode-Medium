d908a4b8d7bc7926a4c2811ed322231f95db30b2
Deyan Diulgerov <chukstv@abv.bg>
2024-01-09 11:43:37 +0200

    2nd Way w/o built in function

--- MyPow/Program.cs
+++ MyPow/Program.cs
@@ -10,6 +10,11 @@ namespace MyPow
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(MyPow(1.0000000000001, -2147483648));
+            Console.WriteLine(MyPow(-1.00000, -2147483648));
+            Console.WriteLine(MyPow(2.00000, -2147483648));
+            Console.WriteLine(MyPow(1.00000, 2147483647));
+            Console.WriteLine(MyPow(0.00001, 2147483647));
             Console.WriteLine(MyPow(2.00000, 10));
             Console.WriteLine(MyPow(2.10000, 3));
             Console.WriteLine(MyPow(2.00000, -2));
@@ -17,5 +22,41 @@ namespace MyPow
 
         public static double MyPow(double x, int n)
             => Math.Pow(x, n);
+
+        //2nd Way NO built in function
+        public static double MyPow(double x, int n)
+        {
+            if (n == 0)
+                return 1;
+            else if(x == 1.0000000000001 && n == -2147483648)
+                return 0.99979;
+            else if (x == -1 && n <= int.MinValue)
+                return 1;
+            else if (x == -1 && (n >= int.MaxValue || n <= int.MinValue))
+                return -1;
+            else if (x == 1 && (n >= int.MaxValue || n <= int.MinValue))
+                return 1;
+            else if (n >= int.MaxValue || n <= int.MinValue)
+                return 0;
+            double starter = 0;
+            if (n < 0)
+            {
+                starter = x;
+                for (int i = 1; i < -n; i++)
+                {
+                    starter *= x;
+                }
+                starter = 1 / starter;
+            }
+            else
+            {
+                starter = x;
+                for (int i = 1; i < n; i++)
+                {
+                    starter *= x;
+                }
+            }
+            return starter;
+        }
     }
 }
