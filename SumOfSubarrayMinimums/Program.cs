13bbbbe4a4e09c1a58e6584a99cec93d46a04b9f
Deyan Diulgerov <chukstv@abv.bg>
2024-01-20 16:04:26 +0200

    Add files via upload

---
+++ SumOfSubarrayMinimums/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfSubarrayMinimums
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SumOfSubarrayMinimums(new int[] { 3, 1, 2, 4 }));
+            Console.WriteLine(SumOfSubarrayMinimums(new int[] { 11, 81, 94, 43, 3 }));
+        }
+        public static int SumOfSubarrayMinimums(int[] arr)
+        {
+            int minSum = 0;
+            int mod = (int)Math.Pow(10, 9) + 7;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int min = int.MaxValue;
+                for (int j = i; j < arr.Length; j++)
+                {
+                    min = Math.Min(min, arr[j]);
+                    minSum += min;
+                }
+                minSum = minSum % mod;
+            }
+            return minSum;
+        }
+    }
+}
