45b1e0a49c42042f58298684f83e95dd21e3f455
Deyan Diulgerov <chukstv@abv.bg>
2024-01-21 12:40:49 +0200

    Add files via upload

---
+++ MinimumAddToMakeParenthesesValid/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinimumAddToMakeParenthesesValid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MinimumAddToMakeParenthesesValid("())"));
+            Console.WriteLine(MinimumAddToMakeParenthesesValid("((("));
+        }
+        public static int MinimumAddToMakeParenthesesValid(string s)
+        {
+            int n = -1;
+
+            while (n != s.Length)
+            {
+                n = s.Length;
+
+                s = s
+                    .Replace("()", "")
+                    .Replace("[]", "")
+                    .Replace("{}", "");
+            }
+            return s.Length;
+        }
+    }
+}
---
+++ MinimumAddToMakeParenthesesValid/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MinimumAddToMakeParenthesesValid")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MinimumAddToMakeParenthesesValid")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("523742a6-d6dc-4f2f-a967-ee70bb045961")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
