53955a482d6a84b3db3fe561b434caf5f0f5e86e
Deyan Diulgerov <chukstv@abv.bg>
2024-02-06 15:22:58 +0200

    2 ways

--- TheKthFactorOfN/Program.cs
+++ TheKthFactorOfN/Program.cs
@@ -14,6 +14,21 @@ namespace TheKthFactorOfN
             Console.WriteLine(KthFactor(7, 2));
             Console.WriteLine(KthFactor(4, 4));
         }
+        //2nd Way stops after k elements
+         public static int KthFactor2(int n, int k)
+        {
+            HashSet<int> set = new HashSet<int>();
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                    set.Add(i);
+                if (set.Count() == k)
+                    return i;
+            }
+            return -1;
+        }
+        //Doesnt stop and finds all elements
+        //Then we return the kth one
         public static int KthFactor(int n, int k)
         {
             HashSet<int> set = new HashSet<int>();
