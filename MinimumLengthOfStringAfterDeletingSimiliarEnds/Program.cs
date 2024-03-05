f7fcf74b84f7715facd00f9e4f464cc946c2930f
Deyan Diulgerov <chukstv@abv.bg>
2024-03-05 13:16:10 +0200

    2 Ways

--- MinimumLengthOfStringAfterDeletingSimiliarEnds/Program.cs
+++ MinimumLengthOfStringAfterDeletingSimiliarEnds/Program.cs
@@ -32,5 +32,28 @@ namespace MinimumLengthOfStringAfterDeletingSimiliarEnds
             }
             return s.Length;
         }
+        //Two Pointers - Four Pointers
+        public static int MinimumLengthOfStringAfterDeletingSimiliarEnds2(string s)
+        {
+            int left = 0, right = s.Length - 1;
+            int secLeft = 1, secRight = s.Length - 2;
+            while (left < right)
+            {
+                if (s[left] != s[right])
+                    break;
+                while (secLeft <= secRight && s[left] == s[secLeft])
+                    secLeft++;
+                while (secLeft <= secRight && s[right] == s[secRight])
+                    secRight--;
+
+                s = s.Remove(secRight + 1, right - secRight);
+                s = s.Remove(left, secLeft);
+                left = 0;
+                right = s.Length - 1;
+                secLeft = left + 1;
+                secRight = right - 1;
+            }
+            return s.Length;
+        }
     }
 }
