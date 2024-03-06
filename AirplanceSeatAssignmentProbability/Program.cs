3fd8a408eca7099b1ab5e4b5a00072bf96bdfbcc
Deyan Diulgerov <chukstv@abv.bg>
2024-03-06 12:09:10 +0200

    Add files via upload

---
+++ AirplanceSeatAssignmentProbability/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirplanceSeatAssignmentProbability
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(AirplanceSeatAssignmentProbability(1));
+            Console.WriteLine(AirplanceSeatAssignmentProbability(2));
+            Console.WriteLine(AirplanceSeatAssignmentProbability(3));
+            Console.WriteLine(AirplanceSeatAssignmentProbability(4));
+            Console.WriteLine(AirplanceSeatAssignmentProbability(5));
+            Console.WriteLine(AirplanceSeatAssignmentProbability(12));
+            Console.WriteLine(AirplanceSeatAssignmentProbability(24));
+        }
+        public static double AirplanceSeatAssignmentProbability(int n)
+            => n == 1 ? n : 0.5;
+    }
+}
