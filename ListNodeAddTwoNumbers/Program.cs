427e8d85d288d8553fbfe9aa6df4f26652dac944
Deyan Diulgerov <chukstv@abv.bg>
2024-03-18 13:51:48 +0200

    Added 2nd Way - My Way

--- ListNodeAddTwoNumbers/Program.cs
+++ ListNodeAddTwoNumbers/Program.cs
@@ -12,7 +12,33 @@ namespace ListNodeAddTwoNumbers
         {
 
         }
-
+        // My Way - 2nd Way
+        public static ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            List<string> list1 = new List<string>();
+            List<string> list2 = new List<string>();
+            while (l1 != null)
+            {
+                list1.Add(l1.val.ToString());
+                l1 = l1.next;
+            }
+            while (l2 != null)
+            {
+                list2.Add(l2.val.ToString());
+                l2 = l2.next;
+            }
+            list1.Reverse();
+            list2.Reverse();
+            BigInteger res1 = BigInteger.Parse(String.Join("", list1));
+            BigInteger res2 = BigInteger.Parse(String.Join("", list2));
+            string str = ((BigInteger)(res1 + res2)).ToString();
+            ListNode result = null;
+            for (int i = 0; i < str.Length; i++)
+            {
+                result = new ListNode(str[i] - 48, result);
+            }
+            return result;
+        }
         public static ListNode ListNodeAddTwoNumbers(ListNode l1, ListNode l2, int carry = 0)
         {
             if (l1 == null && l2 == null && carry == 0) return null;
