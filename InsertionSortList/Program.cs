c66550cc8bce854aa1874937be59988237d50db5
Deyan Diulgerov <chukstv@abv.bg>
2024-03-31 13:47:23 +0300

    Add files via upload

---
+++ InsertionSortList/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsertionSortList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        public static ListNode SortList(ListNode head)
+        {
+            List<int> sorted = new List<int>();
+            while (head != null)
+            {
+                sorted.Add(head.val);
+                head = head.next;
+            }
+            sorted.Sort();
+            sorted.Reverse();
+            ListNode res = null;
+            for (int i = 0; i < sorted.Count(); i++)
+                res = new ListNode(sorted[i], res);
+            return res;
+        }
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+    }
+}
