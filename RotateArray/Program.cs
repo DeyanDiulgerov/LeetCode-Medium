77aab05e7a69af36c6e38bf00596464cc2291127
Deyan Diulgerov <chukstv@abv.bg>
2024-05-22 12:24:46 +0300

    Tidier code

--- RotateArray/Program.cs
+++ RotateArray/Program.cs
@@ -16,23 +16,20 @@ namespace RotateArray
         public static int[] RotateArray(int[] nums, int k)
         {
             int n = nums.Length;
-            k = k % n;
-
-            Reverse(nums, 0, n - 1);
-            Reverse(nums, 0, k - 1);
-            Reverse(nums, k, n - 1);
-
-            return nums;
-        }
-        static void Reverse(int[] nums, int left, int right)
-        {
-            while (left < right)
+            k %= n;
+            Swap(0, n - 1);
+            Swap(0, k - 1);
+            Swap(k, n - 1);
+            void Swap(int left, int right)
             {
-                var temp = nums[left];
-                nums[left] = nums[right];
-                nums[right] = temp;
-                left++;
-                right--;
+                while(left < right)
+                {
+                    int temp = nums[left];
+                    nums[left] = nums[right];
+                    nums[right] = temp;
+                    left++;
+                    right--;
+                }
             }
         }
     }
