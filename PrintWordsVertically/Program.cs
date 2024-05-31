287341cc67047c97dddcbccc1055206ce15f0569
Deyan Diulgerov <chukstv@abv.bg>
2024-05-31 09:33:05 +0300

    Add files via upload

---
+++ PrintWordsVertically/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintWordsVertically
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(String.Join(",", PrintWordsVertically("HOW ARE YOU")));
+            Console.WriteLine(String.Join(",", PrintWordsVertically("TO BE OR NOT TO BE")));
+            Console.WriteLine(String.Join(",", PrintWordsVertically("CONTEST IS COMING")));
+        }
+        public static IList<string> PrintWordsVertically(string s)
+        {
+            string[] splitted = s.Split(' ');
+            int longest = splitted.OrderBy(x => x.Length).Last().Length;
+            string[] result = new string[longest];
+            int counter = 0;
+            while (counter < longest)
+            {
+                string newRes = "";
+                for (int i = 0; i < splitted.Count(); i++)
+                {
+                    if (counter >= splitted[i].Length)
+                        newRes += " ";
+                    else
+                        newRes += splitted[i][counter];
+                }
+                result[counter] = newRes.TrimEnd(' ');
+                counter++;
+            }
+            return result.ToList();
+        }
+    }
+}
